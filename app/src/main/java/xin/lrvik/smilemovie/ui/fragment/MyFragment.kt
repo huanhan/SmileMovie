cc0b4c5a49ea11bc7fbac3a124d16ec00470c6da
豢涵 <huanhanfu@126.com>
2018-11-24 19:22:46 +0800

    完善我的界面功能介绍项

--- app/src/main/java/xin/lrvik/smilemovie/ui/fragment/MyFragment.kt
+++ app/src/main/java/xin/lrvik/smilemovie/ui/fragment/MyFragment.kt
@@ -4,19 +4,41 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bigkoo.alertview.AlertView
 import kotlinx.android.synthetic.main.fragment_my.*
-import lrvik.xin.base.ext.onClick
 import lrvik.xin.base.ui.fragment.BaseFragment
-import org.jetbrains.anko.support.v4.startActivity
 import xin.lrvik.smilemovie.R
+import android.content.Intent
+import android.net.Uri
+
 
 class MyFragment : BaseFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_my,null)
+        return inflater.inflate(R.layout.fragment_my, null)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        mLlEmail.setOnClickListener {
+            val uri = Uri.parse("mailto:huanhanfu@126.com")
+            val it = Intent(Intent.ACTION_SENDTO, uri)
+            startActivity(it)
+        }
 
+        mLlGithub.setOnClickListener {
+            val uri = Uri.parse(context!!.resources.getString(R.string.github))
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
+
+        mLlFollow.setOnClickListener {
+            AlertView("提示", "暂无公众号", null, listOf("确定").toTypedArray(), null, activity,
+                    AlertView.Style.Alert, null).show()
+        }
+
+        mLlMainZe.setOnClickListener {
+            AlertView("免责声明", context!!.resources.getString(R.string.mianze), null, listOf("确定").toTypedArray(), null, activity,
+                    AlertView.Style.Alert, null).show()
+        }
     }
 }
